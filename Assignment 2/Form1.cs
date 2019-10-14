b2b1ccdc6781fbcff396f9cc5c6d6b048369c525
Milosz Telinski <rc0037857@student.riversidecollege.ac.uk>
2019-10-14 15:09:47 +0100

    done

--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -24,26 +24,32 @@ namespace Assignment_2
             try
             {
                 r1 = double.Parse(textBox1.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r1 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
             double r2;
 
             try
             {
                 r2 = double.Parse(textBox2.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r2 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
 
             double tot1 = r1 + r2;
-            label1.Text = "Total Resistance = " + tot1 + " Ω";
+            //This carries out the calculation to find out the total resistance of 2 resistors in a series circuit
+            label1.Text = "Total Resistance = " + tot1.ToString("N3") + " Ω";
+            //This prints the total resistance and it gives the answer to 3 decimal places
         }
 
         private void Label2_Click(object sender, EventArgs e)
@@ -58,26 +64,33 @@ namespace Assignment_2
             try
             {
                 r3 = double.Parse(textBox3.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r3 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
+
             double r4;
 
             try
             {
                 r4 = double.Parse(textBox4.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r4 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
 
             double tot2 = (1 / ((1.0 / r3) + (1.0 / r4)));
-            label2.Text = "Total Resistance = " + tot2 + " Ω";
+            //This carries out the calculation to find out the total resistance of 2 resistors in a paralell circuit
+            label2.Text = "Total Resistance = " + tot2.ToString("N3") + " Ω";
+            //This prints the total resistance and it gives the answer to 3 decimal places
         }
 
         private void Button3_Click(object sender, EventArgs e)
@@ -87,38 +100,47 @@ namespace Assignment_2
             try
             {
                 r5 = double.Parse(textBox5.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r5 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
+
             double r6;
 
             try
             {
                 r6 = double.Parse(textBox6.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r6 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
 
             double r7;
 
             try
             {
                 r7 = double.Parse(textBox7.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r7 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
 
             double tot3 = r5 + r6+ r7;
-            label3.Text = "Total Resistance = " + tot3 + " Ω";
+            //This carries out the calculation to find out the total resistance of 3 resistors in a series circuit
+            label3.Text = "Total Resistance = " + tot3.ToString("N3") + " Ω";
+            //This prints the total resistance and it gives the answer to 3 decimal places
         }
 
         private void Label4_Click(object sender, EventArgs e)
@@ -133,39 +155,47 @@ namespace Assignment_2
             try
             {
                 r8 = double.Parse(textBox8.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r8 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
+
             double r9;
 
             try
             {
                 r9 = double.Parse(textBox9.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r9 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
 
             double r10;
 
             try
             {
                 r10 = double.Parse(textBox10.Text);
+                //This converts the numbers in the textbox into a double variable
             }
             catch
             {
                 MessageBox.Show("Not a correct value.");
                 r10 = 0.0;
             }
+            //The try catch checks if the input is an integer and not a string
 
             double tot4 = (1 / ((1.0 / r8) + ((1.0 / r9) + (1.0 / r10))));
-            double tot4 = tot4.ToString("N3");
-            label4.Text = "Total Resistance = " + tot4 + " Ω";
+            //This carries out the calculation to find out the total resistance of 3 resistors in a paralell circuit
+            label4.Text = "Total Resistance = " + tot4.ToString("N3") + " Ω";
+            //This prints the total resistance and it gives the answer to 3 decimal places
         }
     }
 }
